eee76fa2c76829380190696c6ca55c919fe04a7d
Kevin Burke <kevin@twilio.com>
2012-11-23 23:31:45 -0800

    Bump PEAR library version to 3.8.1

--- bump_version.sh
+++ bump_version.sh
@@ -8,6 +8,7 @@ VERSION="$1";
 GZIPPED_REPO="Services_Twilio-$VERSION.tgz"
 
 pushd ../twilio-php;
+    pear install XML_Serializer-0.20.2 PEAR_PackageFileManager_Plugins PEAR_PackageFileManager2
     pear channel-discover twilio.github.com/pear
     php package.php > package.xml;
     pear package;
